dac87210d23cfc0f83bc8402cd2c0c40e6defaf9
Alireza B <Alireza@simor.org>
2020-06-03 13:21:24 -0700

    added tabs

--- App.js
+++ App.js
@@ -2,46 +2,51 @@ import * as React from 'react';
 import { Text, View, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>all opinions</Text>
-    </View>
-  );
-}
-
-function AddPostScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-
-      <TouchableOpacity>
-      <Text> Add Post </Text>
-      </TouchableOpacity>
-
-    </View>
-  );
-}
-
-function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile</Text>
-    </View>
-  );
-}
-
+import { RNCamera, FaceDetector } from 'react-native-camera';
+import AddPost from './component/AddPost';
+import Home from './component/Home';
+import Profile from './component/Profile';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createStackNavigator} from '@react-navigation/stack';
 
 const Tab = createBottomTabNavigator();
 
+function Tabs (){
+  return (
+    <Tab.Navigator >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }} />
+
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="plus" color={color} size={size} />
+        ),
+      }}  />
+    </Tab.Navigator>
+  
+  )
+}
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Add Post" component={AddPostScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+
+        <NavigationContainer >
+          <Stack.Navigator>
+              <Stack.Screen name="Home" component={Tabs}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+
+    );
 }
